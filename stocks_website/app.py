cf13ffe340bd83283706a2a192a0832695d41b83
alexandra <ajane717@gmail.com>
2019-12-02 17:08:00 +0200

    a message

--- stocks_website/app.py
+++ stocks_website/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
 from stocks_website import routes
 
 app = Flask(__name__)
 
 app.register_blueprint(routes.main_routes)
 
+if __name__ == '__main__':
+    app.run()
--- stocks_website/routes.py
+++ stocks_website/routes.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template
 
-main_routes = Blueprint('main', __name__, 'template/')
+main_routes = Blueprint('main', __name__, template_folder='template/')
+
 
 @main_routes.route('/')
 def index():
+    """@main_routes.route('/home/<username>')
+def index(username):"""
+    """template = '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, {{ name }}!</h1>
+        </body>
+    </html>'''
+    html = flask.render_template_string( template, name=username )
+    return html"""
     return render_template('index.html', name='Jane')
